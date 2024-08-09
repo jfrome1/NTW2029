b8442cfd1de77048af0fb252098a66f24519de75
jfrome1 <78241151+jfrome1@users.noreply.github.com>
2024-08-09 11:45:47 +0800

    chore: add remark-external-links plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import starlight from '@astrojs/starlight';
 // import starlightLinksValidator from 'starlight-links-validator';
 import starlightUtils from '@lorenzo_lewis/starlight-utils';
 import markdoc from '@astrojs/markdoc';
+import remarkExternalLinks from 'remark-external-links';
 
 export default defineConfig({
-
+//  site: 'https://jfrome1.github.io/ntw2029/', // GitHub Pages URL config
+//  base: '/ntw2029/',
   integrations: [
     starlight({
       plugins: [
@@ -25,10 +27,10 @@ export default defineConfig({
     		label: 'NTW2029',
 	    	// Don't collapse the main group by default.
 			collapsed: false,
-			autogenerate: { directory: 'ntw2029', collapsed: true },
+			autogenerate: { directory: 'course-ntw2029', collapsed: true },
         },
 		{
-			label: 'General course info',
+			label: 'General',
 			// Don't collapse the main group by default.
 			collapsed: false,
 			autogenerate: { directory: 'courses', collapsed: true },
@@ -57,4 +59,9 @@ export default defineConfig({
     }),
     markdoc()
   ],
+  markdown: {
+    remarkPlugins: [
+      [remarkExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+    ]
+  }
 });
