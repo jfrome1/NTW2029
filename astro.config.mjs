ff84104d1ae6c0e55b4cce8be0bff4d6d608b924
unknown <joeyk2k@gmail.com>
2024-10-15 08:12:14 +0545

    commented remark

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from "@astrojs/starlight";
 // import starlightLinksValidator from "starlight-links-validator";
 //import starlightUtils from "@lorenzo_lewis/starlight-utils";
 // import markdoc from '@astrojs/markdoc';
-import remarkExternalLinks from "remark-external-links";
+// import remarkExternalLinks from "remark-external-links";
 import starlightNutshell from "starlight-nutshell";
 import rehypeExternalLinks from "rehype-external-links";
 
@@ -60,52 +60,52 @@ export default defineConfig({
           collapsed: true,
           items: [
             {
-				label: "General information",
-				collapsed: true,
-				items: [
-				"course-ntw2029/assignments/general/feedback",
-				"course-ntw2029/assignments/general/ex-guidelines",
-				"course-ntw2029/assignments/general/paper-guidelines",
-				"course-ntw2029/assignments/general/formatting",
-				],
-			  },
-			  {
-				label: "Exercises",
-				collapsed: true,
-				items: [
-					"course-ntw2029/assignments/exercises/e01-introductions",
-					"course-ntw2029/assignments/exercises/e02-explain-something",
-					"course-ntw2029/assignments/exercises/e03-boyd",
-					"course-ntw2029/assignments/exercises/e04-goldfinch",
-					"course-ntw2029/assignments/exercises/e05-p1-conf-notes",
-					"course-ntw2029/assignments/exercises/e06-p1-writing-reflection",
-					"course-ntw2029/assignments/exercises/e07-class-participation",
-					"course-ntw2029/assignments/exercises/e08-p2-proposal-conf-notes",
-					"course-ntw2029/assignments/exercises/e09-p2-draft-conf-notes",
-					"course-ntw2029/assignments/exercises/e10-workload",
-					"course-ntw2029/assignments/exercises/e11-p2-writing-reflection",
-				],
-			  },
-			  {
-				label: "Papers",
-				collapsed: true,
-				items: [
-					"course-ntw2029/assignments/papers/p1",
-					"course-ntw2029/assignments/papers/p1-outline",
-					//"course-ntw2029/assignments/papers/p1-draft",
-					"course-ntw2029/assignments/papers/p1-final",
-					"course-ntw2029/assignments/papers/p2-1-overview",
-					"course-ntw2029/assignments/papers/p2-2-general-proposal",
-					"course-ntw2029/assignments/papers/p2-3-academicconversation",
-					"course-ntw2029/assignments/papers/p2-4-brief-source-summary",
-					"course-ntw2029/assignments/papers/p2-5-source-summary-outline",
-					"course-ntw2029/assignments/papers/p2-6-proposal-draft-outline",
-					"course-ntw2029/assignments/papers/p2-7-intro-full-outline",
-					"course-ntw2029/assignments/papers/p2-8-full-draft-and-outline",
-					"course-ntw2029/assignments/papers/p2-9-final",
-				],
-			  },
-			],
+              label: "General information",
+              collapsed: true,
+              items: [
+                "course-ntw2029/assignments/general/feedback",
+                "course-ntw2029/assignments/general/ex-guidelines",
+                "course-ntw2029/assignments/general/paper-guidelines",
+                "course-ntw2029/assignments/general/formatting",
+              ],
+            },
+            {
+              label: "Exercises",
+              collapsed: true,
+              items: [
+                "course-ntw2029/assignments/exercises/e01-introductions",
+                "course-ntw2029/assignments/exercises/e02-explain-something",
+                "course-ntw2029/assignments/exercises/e03-boyd",
+                "course-ntw2029/assignments/exercises/e04-goldfinch",
+                "course-ntw2029/assignments/exercises/e05-p1-conf-notes",
+                "course-ntw2029/assignments/exercises/e06-p1-writing-reflection",
+                "course-ntw2029/assignments/exercises/e07-class-participation",
+                "course-ntw2029/assignments/exercises/e08-p2-proposal-conf-notes",
+                "course-ntw2029/assignments/exercises/e09-p2-draft-conf-notes",
+                "course-ntw2029/assignments/exercises/e10-workload",
+                "course-ntw2029/assignments/exercises/e11-p2-writing-reflection",
+              ],
+            },
+            {
+              label: "Papers",
+              collapsed: true,
+              items: [
+                "course-ntw2029/assignments/papers/p1",
+                "course-ntw2029/assignments/papers/p1-outline",
+                //"course-ntw2029/assignments/papers/p1-draft",
+                "course-ntw2029/assignments/papers/p1-final",
+                "course-ntw2029/assignments/papers/p2-1-overview",
+                "course-ntw2029/assignments/papers/p2-2-general-proposal",
+                "course-ntw2029/assignments/papers/p2-3-academicconversation",
+                "course-ntw2029/assignments/papers/p2-4-brief-source-summary",
+                "course-ntw2029/assignments/papers/p2-5-source-summary-outline",
+                "course-ntw2029/assignments/papers/p2-6-proposal-draft-outline",
+                "course-ntw2029/assignments/papers/p2-7-intro-full-outline",
+                "course-ntw2029/assignments/papers/p2-8-full-draft-and-outline",
+                "course-ntw2029/assignments/papers/p2-9-final",
+              ],
+            },
+          ],
         },
         {
           label: "Resources",
@@ -143,17 +143,19 @@ export default defineConfig({
     }),
   ],
   markdown: {
-    remarkPlugins: [
-      [
-        remarkExternalLinks,
-        { target: "_blank", rel: ["noopener", "noreferrer"] },
-      ],
-    ],
+    // remarkPlugins: [
+    //   [
+    //     remarkExternalLinks,
+    //     { target: "_blank", rel: ["noopener", "noreferrer"] },
+    //   ],
+    // ],
     rehypePlugins: [
       [
         rehypeExternalLinks,
         {
           content: { type: "text", value: " 🡕" },
+          target: "_blank",
+          rel: ["noopener", "noreferrer"],
         },
       ],
     ],
