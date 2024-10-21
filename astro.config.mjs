fa1270cae5128198ad874a6b27882e19be492ac9
unknown <joeyk2k@gmail.com>
2024-10-21 21:00:28 +0545

    feat: added data and message for feedback

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,139 +9,138 @@ import rehypeExternalLinks from "rehype-external-links";
 
 import compress from "astro-compress";
 
+import inoxToolsContentUtils from "@inox-tools/content-utils";
+
 export default defineConfig({
   // site: 'https://jfrome1.github.io/ntw2029/', // GitHub Pages URL config
   // base: '/ntw2029/',
-  integrations: [
-    starlight({
-      tableOfContents: {
-        minHeadingLevel: 1,
-        maxHeadingLevel: 2,
+  integrations: [starlight({
+    tableOfContents: {
+      minHeadingLevel: 1,
+      maxHeadingLevel: 2,
+    },
+    plugins: [
+      // starlightLinksValidator(),
+      starlightNutshell(),
+      // starlightUtils({
+      //   multiSidebar: {
+      //     switcherStyle: "dropdown",
+      //   },
+      // }),
+    ],
+    title: "NTW2029 - Home",
+    components: {
+      // Override the default components.
+      TableOfContents: "./src/components/CustomTableOfContents.astro",
+      Pagination: "./src/components/CustomPagination.astro",
+    },
+    customCss: ["./src/styles/custom.css"],
+    sidebar: [
+      {
+        link: "course-ntw2029/schedule",
+        label: "Course schedule",
       },
-      plugins: [
-        // starlightLinksValidator(),
-        starlightNutshell(),
-        // starlightUtils({
-        //   multiSidebar: {
-        //     switcherStyle: "dropdown",
-        //   },
-        // }),
-      ],
-      title: "NTW2029 - Home",
-      components: {
-        // Override the default components.
-        TableOfContents: "./src/components/CustomTableOfContents.astro",
-        Pagination: "./src/components/CustomPagination.astro",
+      {
+        label: "Course information",
+        collapsed: true,
+        items: [
+          "course-ntw2029/course-info/start-here",
+          "course-ntw2029/course-info/instructor",
+          "course-ntw2029/course-info/need-help",
+          "course-ntw2029/course-info/readings",
+          "course-ntw2029/course-info/conferences",
+          "course-ntw2029/course-info/gen-ai-policy",
+          "course-ntw2029/course-info/tech-guidelines",
+          "course-ntw2029/course-info/policies",
+          "course-ntw2029/course-info/grading",
+          "course-ntw2029/course-info/extensions",
+        ],
       },
-      customCss: ["./src/styles/custom.css"],
-      sidebar: [
-        {
-          link: "course-ntw2029/schedule",
-          label: "Course schedule",
-        },
-        {
-          label: "Course information",
-          collapsed: true,
-          items: [
-            "course-ntw2029/course-info/start-here",
-            "course-ntw2029/course-info/instructor",
-            "course-ntw2029/course-info/need-help",
-            "course-ntw2029/course-info/readings",
-            "course-ntw2029/course-info/conferences",
-            "course-ntw2029/course-info/gen-ai-policy",
-            "course-ntw2029/course-info/tech-guidelines",
-            "course-ntw2029/course-info/policies",
-            "course-ntw2029/course-info/grading",
-            "course-ntw2029/course-info/extensions",
-          ],
-        },
-        {
-          label: "Assignments",
-          collapsed: true,
-          items: [
-            {
-              label: "General information",
-              collapsed: true,
-              items: [
-                "course-ntw2029/assignments/general/feedback",
-                "course-ntw2029/assignments/general/ex-guidelines",
-                "course-ntw2029/assignments/general/paper-guidelines",
-                "course-ntw2029/assignments/general/formatting",
-              ],
-            },
-            {
-              label: "Exercises",
-              collapsed: true,
-              items: [
-                "course-ntw2029/assignments/exercises/e01-introductions",
-                "course-ntw2029/assignments/exercises/e02-explain-something",
-                "course-ntw2029/assignments/exercises/e03-boyd",
-                "course-ntw2029/assignments/exercises/e04-goldfinch",
-                "course-ntw2029/assignments/exercises/e05-p1-conf-notes",
-                "course-ntw2029/assignments/exercises/e06-p1-writing-reflection",
-                "course-ntw2029/assignments/exercises/e07-class-participation",
-                "course-ntw2029/assignments/exercises/e08-p2-proposal-conf-notes",
-                "course-ntw2029/assignments/exercises/e09-p2-draft-conf-notes",
-                "course-ntw2029/assignments/exercises/e10-workload",
-                "course-ntw2029/assignments/exercises/e11-p2-writing-reflection",
-              ],
-            },
-            {
-              label: "Papers",
-              collapsed: true,
-              items: [
-                "course-ntw2029/assignments/papers/p1",
-                "course-ntw2029/assignments/papers/p1-outline",
-                //"course-ntw2029/assignments/papers/p1-draft",
-                "course-ntw2029/assignments/papers/p1-final",
-                "course-ntw2029/assignments/papers/p2-1-overview",
-                "course-ntw2029/assignments/papers/p2-2-general-proposal",
-                "course-ntw2029/assignments/papers/p2-3-academicconversation",
-                "course-ntw2029/assignments/papers/p2-4-brief-source-summary",
-                "course-ntw2029/assignments/papers/p2-5-source-summary-outline",
-                "course-ntw2029/assignments/papers/p2-6-proposal-draft-outline",
-                "course-ntw2029/assignments/papers/p2-7-intro-full-outline",
-                "course-ntw2029/assignments/papers/p2-8-full-draft-and-outline",
-                "course-ntw2029/assignments/papers/p2-9-final",
-              ],
-            },
-          ],
-        },
-        {
-          label: "Resources",
-          collapsed: true,
-          items: [
-            {
-              label: "Evolution",
-              collapsed: true,
-              items: [
-                "course-ntw2029/resources/evolution/ev-religion",
-                "course-ntw2029/resources/evolution/ev-resources",
-                "course-ntw2029/resources/evolution/nct",
-              ],
-            },
-            {
-              label: "Writing",
-              collapsed: true,
-              items: [
-                "course-ntw2029/resources/writing/citations",
-                "course-ntw2029/resources/writing/grammar",
-                "course-ntw2029/resources/writing/software",
-                "course-ntw2029/resources/writing/topic-sentences",
-              ],
-            },
-          ],
-        },
-      ],
-    }),
-    compress({
-      HTML: {
-        "html-minifier-terser": {
-          removeComments: true,
-        },
+      {
+        label: "Assignments",
+        collapsed: true,
+        items: [
+          {
+            label: "General information",
+            collapsed: true,
+            items: [
+              "course-ntw2029/assignments/general/feedback",
+              "course-ntw2029/assignments/general/ex-guidelines",
+              "course-ntw2029/assignments/general/paper-guidelines",
+              "course-ntw2029/assignments/general/formatting",
+            ],
+          },
+          {
+            label: "Exercises",
+            collapsed: true,
+            items: [
+              "course-ntw2029/assignments/exercises/e01-introductions",
+              "course-ntw2029/assignments/exercises/e02-explain-something",
+              "course-ntw2029/assignments/exercises/e03-boyd",
+              "course-ntw2029/assignments/exercises/e04-goldfinch",
+              "course-ntw2029/assignments/exercises/e05-p1-conf-notes",
+              "course-ntw2029/assignments/exercises/e06-p1-writing-reflection",
+              "course-ntw2029/assignments/exercises/e07-class-participation",
+              "course-ntw2029/assignments/exercises/e08-p2-proposal-conf-notes",
+              "course-ntw2029/assignments/exercises/e09-p2-draft-conf-notes",
+              "course-ntw2029/assignments/exercises/e10-workload",
+              "course-ntw2029/assignments/exercises/e11-p2-writing-reflection",
+            ],
+          },
+          {
+            label: "Papers",
+            collapsed: true,
+            items: [
+              "course-ntw2029/assignments/papers/p1",
+              "course-ntw2029/assignments/papers/p1-outline",
+              //"course-ntw2029/assignments/papers/p1-draft",
+              "course-ntw2029/assignments/papers/p1-final",
+              "course-ntw2029/assignments/papers/p2-1-overview",
+              "course-ntw2029/assignments/papers/p2-2-general-proposal",
+              "course-ntw2029/assignments/papers/p2-3-academicconversation",
+              "course-ntw2029/assignments/papers/p2-4-brief-source-summary",
+              "course-ntw2029/assignments/papers/p2-5-source-summary-outline",
+              "course-ntw2029/assignments/papers/p2-6-proposal-draft-outline",
+              "course-ntw2029/assignments/papers/p2-7-intro-full-outline",
+              "course-ntw2029/assignments/papers/p2-8-full-draft-and-outline",
+              "course-ntw2029/assignments/papers/p2-9-final",
+            ],
+          },
+        ],
       },
-    }),
-  ],
+      {
+        label: "Resources",
+        collapsed: true,
+        items: [
+          {
+            label: "Evolution",
+            collapsed: true,
+            items: [
+              "course-ntw2029/resources/evolution/ev-religion",
+              "course-ntw2029/resources/evolution/ev-resources",
+              "course-ntw2029/resources/evolution/nct",
+            ],
+          },
+          {
+            label: "Writing",
+            collapsed: true,
+            items: [
+              "course-ntw2029/resources/writing/citations",
+              "course-ntw2029/resources/writing/grammar",
+              "course-ntw2029/resources/writing/software",
+              "course-ntw2029/resources/writing/topic-sentences",
+            ],
+          },
+        ],
+      },
+    ],
+  }), compress({
+    HTML: {
+      "html-minifier-terser": {
+        removeComments: true,
+      },
+    },
+  }), inoxToolsContentUtils()],
   markdown: {
     // remarkPlugins: [
     //   [
