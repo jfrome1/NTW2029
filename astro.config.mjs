be719271559a77b013624eb223e5fa74fb6d0959
unknown <joeyk2k@gmail.com>
2025-04-29 16:59:51 +0700

    disabled heading links

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
         minHeadingLevel: 1,
         maxHeadingLevel: 2,
       },
+      markdown: {
+        headingLinks: false,
+      },
       plugins: [
         starlightLinksValidator({
           errorOnFallbackPages: false,
